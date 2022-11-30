0967ff6b8e8cdb6325abc21452d9044cc0243900
Antonio Bruno <abruno@artiso.com>
2022-11-30 22:33:55 +0100

    improvements

--- init/configure-stack.sh
+++ init/configure-stack.sh
@@ -7,28 +7,32 @@ until curl -u elastic:changeme -s http://elasticsearch:9200/_cat/health -o /dev/
     sleep 1
 done
 
-echo "Setting Elasticsearch password to ${ES_PASSWORD}"
-curl -s -XPUT -u elastic:changeme 'elasticsearch:9200/_xpack/security/user/elastic/_password' -H "Content-Type: application/json" -d "{
-  \"password\" : \"${ES_PASSWORD}\"
-}"
-
-curl -s -XPUT -u elastic:${ES_PASSWORD} 'elasticsearch:9200/_xpack/security/user/kibana/_password' -H "Content-Type: application/json" -d "{
-  \"password\" : \"${ES_PASSWORD}\"
-}"
-
-curl -s -XPUT -u elastic:${ES_PASSWORD} 'elasticsearch:9200/_xpack/security/user/logstash_system/_password' -H "Content-Type: application/json" -d "{
-  \"password\" : \"${ES_PASSWORD}\"
-}"
-
-
-# Wait for Kibana to start up before doing anything.
-until curl -s http://kibana:5601/login -o /dev/null; do
+# Waiting for Kibana to start up before doing anything.
+echo Waiting for Kibana...
+until curl -f -LI http://kibana:5601/api/rollup/indices; do
     echo Waiting for Kibana...
     sleep 1
 done
 
+echo Creating index-pattern on Kibana...
+curl -X POST http://kibana:5601/api/saved_objects/index-pattern \
+  -H 'kbn-xsrf: true' \
+  -H 'Content-Type: application/json' \
+  -d "{\"attributes\": { \"title\": \"${ES_FILEBEAT_INDEX_NAME}*\", \"timeFieldName\": \"@timestamp\" }}";
 
-# Set the default index pattern.
-echo Setup default index on Elastic...
-curl -s -XPUT http://elastic:${ES_PASSWORD}@elasticsearch:9200/.kibana/config/${ELASTIC_VERSION} \
-     -d "{\"defaultIndex\" : \"${ES_DEFAULT_INDEX_PATTERN}\"}"
+echo Setup default index-pattern on Kibana...
+curl -X POST http://localhost:5601/api/kibana/settings \
+  -H 'kbn-xsrf: true' \
+  -H 'Content-Type: application/json' \
+  -d "{\"changes\":{\"defaultIndex\":\"${ES_FILEBEAT_INDEX_NAME}-*\"}}"
+
+echo Increase the max_analyzed_offset for big messages...
+curl -X PUT "http://elasticsearch:9200/${ES_FILEBEAT_INDEX_NAME}/_settings"  \
+  -H 'Content-Type: application/json' \
+  -d "{ \"index\" : { \"highlight.max_analyzed_offset\" : ${ES_MAX_ANALYZED_OFFSET}}}"
+
+
+#echo Setup mapping on Elastic...
+# curl -s -XPUT http://elasticsearch:5601/_template/template_logs?include_type_name=true \
+#     -H "Content-Type: application/json"  \
+#     -d ''
